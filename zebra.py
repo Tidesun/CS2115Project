1d2d55e686e4df013ffe24f8cafcc3ffdd093be0
Tidesun <lihaoran9836@gmail.com>
2017-11-28 16:54:54 +0800

    Update Zebra

--- zebra.py
+++ zebra.py
@@ -1,36 +1,37 @@
 from pyeda.inter import *
-
+ 
 name = [['Norwegian', 'Ukrainian', 'Englishman', 'Spaniard', 'Japanese'],
 	['Fox', 'Horse', 'Snails', 'Dog', 'Zebra'],
 	['Water', 'Tea', 'Milk', 'Orange Juice', 'Coffee'],
 	['Yellow', 'Blue', 'Red', 'Ivory', 'Green'],
 	['Kools', 'Chesterfield', 'Old Gold', 'Lucky Strike', 'Parliament']]
-
+ 
 info="Nationality Pets Drinks Color Smokes".split()
-
+ 
 # X[itemcatagory][itemname][housed]
 X = exprvars('X',(1, 6),(1, 6),(1, 6))
-
+ 
 A,B,C,D,E,F,G,H,I,J,K,L=map(exprvar,'ABCDEFGHIJKL')
-
+ 
 # Every item has unique house
-
+ 
 V = And(*[
         And(*[
             OneHot(*[ X[r, c, v]
                 for v in range(1, 6) ])
             for c in range(1, 6) ])
         for r in range(1, 6) ])
-
+ 
 # Every house has unique item
+ 
 W = And(*[
         And(*[
             OneHot(*[ X[r, c, v]
                 for c in range(1, 6) ])
             for v in range(1, 6) ])
         for r in range(1, 6) ])
-
-
+ 
+ 
 # 2.The Englishman lives in the red house.
 A=And(*[Equal(X[1,3,i],X[4,3,i]) for i in range(1, 6) ])
 # 3.The Spaniard owns the dog.
@@ -47,9 +48,9 @@ F=And(*[Equal(X[5,3,i],X[2,3,i]) for i in range(1, 6) ])
 # 8.Kools are smoked in the yellow house.
 G=And(*[Equal(X[5,1,i],X[4,1,i]) for i in range(1, 6) ])
 # 9.Milk is drunk in the middle house.
-#=True
+ 
 # 10.The Norwegian lives in the first house.
-#X[1,1,1]=True
+ 
 # 11.The man who smokes Chesterfields lives in the house next to the man with the fox.
 H1=exprvar('H1')
 H2=exprvar('H2')
@@ -78,21 +79,19 @@ L1=L1&(~X[1,1,1])
 L2=And(*[Equal(X[1,1,i-1],X[4,2,i]) for i in range(2, 6)])
 L2=L2&(~X[1,1,5])
 L=L1|L2
+ 
 S=A&B&C&D&E&F&G&H&I&J&K&L&V&W&(X[3,3,3])&(X[1,1,1])
+ 
 result=S.satisfy_one()
-
+ 
 for i in range(5):
-
+ 
 	print("Person who lives in house No.",i+1,":")
-
+ 
 	for k in range(5):
 		for j in range (5):
 			if (result[X[k+1,j+1,i+1]]==1):
 				print(info[k],':',name[k][j])
-
+ 
 	print()
-
-
-
-
 
